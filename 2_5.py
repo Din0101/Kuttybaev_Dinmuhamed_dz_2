ab5b06558808b97d0d4fd79bb4716a1e8aa399d3
Din <dinmuhamed50@gmail.com>
2022-04-15 09:21:03 +0600

    dz

--- 2_5.py
+++ 2_5.py
@@ -1,12 +1,15 @@
 some_lst = [57.8, 46.51, 97, 55.6, 6, 77.08, 88.8, 106.6, 77.77, 33.5]
+print(id(some_lst))
 print(some_lst)
 some_str = ''
 for ind, elem in enumerate(sorted(some_lst)):
     some_str += str(int(elem)).zfill(2) + ' руб ' + str(int(elem * 100) % 100).zfill(2) + ' коп, '
 print(some_str)
+print(id(some_lst))
 some_str = ''
 for ind, elem in enumerate(sorted(some_lst, reverse=True)):       # 5 самых больших чисел
     some_str += str(int(elem)).zfill(2) + ' руб ' + str(int(elem * 100) % 100).zfill(2) + ' коп, '
     if ind == 4:
         break
 print(some_str)
+print(id(some_lst))
